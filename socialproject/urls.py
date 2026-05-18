46235b39669b8366f0575191356d0c15021aa3ea
Amit <amitbju4@gmail.com>
2026-05-18 14:19:34 +0530

    Added root URL redirect to feed

--- socialproject/urls.py
+++ socialproject/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
+    path('', RedirectView.as_view(url='posts/feed/'), name='home_redirect'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
